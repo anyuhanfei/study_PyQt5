304d0b801cc251da0f2ae2baf944845506952e2d
anyuhanfei <1223050251@qq.com>
2020-04-09 14:30:35 +0800

    238~239 QSlider

--- 238~239-QSlider/238-QSlider-功能作用和信号.py
+++ 238~239-QSlider/238-QSlider-功能作用和信号.py
@@ -1,17 +1,43 @@
 '''
+238-QSlider-功能作用和信号
 
+垂直或水平滑块
+它允许用户沿水平或垂直凹槽移动滑块手柄，并将手柄的位置转换为合法范围内的整数值
+
+继承自 QAbstractSlider
+
+API:
+    刻度控制:
+        setTickPosition(self, QSlider.TickPosition)
+        sd.tickPosition() -> QSlider.TickPosition
+        sd.setTickInterval(int) -- 这是值间隔，而不是像素间隔。如果为0，滑块将在singleStep和pageStep之间进行选择
+        sd.tickInterval() -> int
+
+附:
+    QSlider.NoTicks -- 0 -- 不要画任何刻度线。
+    QSlider.TicksBothSides -- 3 -- 在凹槽两侧画刻度线。
+    QSlider.TicksAbove -- 1 -- 在（水平）滑块上方绘制刻度线
+    QSlider.TicksBelow -- 2 -- 在（水平）滑块下方绘制刻度线
+    QSlider.TicksLeft -- 1 -- 在（垂直）滑块的左侧绘制刻度线
+    QSlider.TicksRight -- 2 -- 在（垂直）滑块右侧绘制刻度线
 '''
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QSlider
 
 
 class Window(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.setWindowTitle('')
+        self.setWindowTitle('238-QSlider-功能作用和信号')
         self.resize(1000, 500)
+
+        s = QSlider(self)
+        s.move(10, 10)
+
+        '''刻度'''
+        s.setTickPosition(QSlider.TicksBothSides)
 
 
 if __name__ == "__main__":
--- 238~239-QSlider/239-QSlider-案例.py
+++ 238~239-QSlider/239-QSlider-案例.py
@@ -1,17 +1,38 @@
 '''
+239-QSlider-案例
 
+在滑块移动时, 通过标签, 展示滑块当前的数值
+并要求标签的位置, 一直在滑块所在位置的中间
 '''
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QSlider
 
 
 class Window(QWidget):
+    label_x = 16
+    label_y = 82
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.setWindowTitle('')
+        self.setWindowTitle('239-QSlider-案例')
         self.resize(1000, 500)
+
+        self.slider = QSlider(self)
+        self.slider.move(10, 10)
+        self.slider.setMaximum(9)
+
+        self.label = QLabel(self)
+        self.label.move(self.label_x, self.label_y)
+        self.label.setText(str(self.slider.value()))
+
+        self.slider.valueChanged.connect(self._move_slider)
+
+    def _move_slider(self, val):
+        '''修改标签的值和位置'''
+        self.label.setText(str(self.slider.value()))
+        self.label.move(self.label_x, self.label_y - val * 8)
 
 
 if __name__ == "__main__":
