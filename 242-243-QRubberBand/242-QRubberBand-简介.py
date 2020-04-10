3bede4328d4509968606395f88a9184dee283cb3
anyuhanfei <1223050251@qq.com>
2020-04-10 13:59:53 +0800

    242~243-QRubberBand

--- 242-243-QRubberBand/242-QRubberBand-简介.py
+++ 242-243-QRubberBand/242-QRubberBand-简介.py
@@ -1,9 +1,35 @@
 '''
 242-QRubberBand-简介
+
+提供一个矩形或线来指示选择或边界
+一般结合鼠标事件一同协作
+
+继承自 QWidget
+
+构造函数:
+    QRubberBand(QRubberBand.Shape, QWidget)
+
+API:
+    移动
+        move(x, y)
+        move(QPoint)
+    调整大小:
+        resize(width, height)
+        resize(QSize)
+    统一设置:
+        setGeometry(int x, int y, int width, int height)
+        setGeometry(QRect rect)
+    形状获取:
+        shape() -> QRubberBand.Shape
+
+附:
+    QRubberBand.Shape
+        QRubberBand.Line
+        QRubberBand.Rectangle
 '''
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QRubberBand
 
 
 class Window(QWidget):
@@ -12,6 +38,12 @@ class Window(QWidget):
 
         self.setWindowTitle('242-QRubberBand-简介')
         self.resize(1000, 500)
+
+        rb = QRubberBand(QRubberBand.Rectangle, self)
+
+        '''统一设置大小'''
+        rb.setGeometry(10, 10, 60, 60)
+        rb.setVisible(True)  # 默认不显示
 
 
 if __name__ == "__main__":
--- 242-243-QRubberBand/243-QRubberBand-案例.py
+++ 242-243-QRubberBand/243-QRubberBand-案例.py
@@ -1,9 +1,12 @@
 '''
 243-QRubberBand-案例
+
+在一个空白窗口, 展示多个复选框控件, 通过橡皮筋实现批量选中与取消选中效果
 '''
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QCheckBox, QRubberBand
+from PyQt5.QtCore import QRect, QSize
 
 
 class Window(QWidget):
@@ -12,6 +15,37 @@ class Window(QWidget):
 
         self.setWindowTitle('243-QRubberBand-案例')
         self.resize(1000, 500)
+
+        for i in range(0, 36):
+            cb = QCheckBox(self)
+            cb.move(i % 6 * 40 + 10, i // 6 * 40 + 10)  # X 轴用取余计算, Y 轴用整除计算
+            cb.setText(str(i))
+
+    def mousePressEvent(self, evt):
+        '''鼠标按下时执行'''
+        # 创建一个橡皮筋选中控件
+        self.rb = QRubberBand(QRubberBand.Rectangle, self)
+        # 鼠标按下的位置
+        self.起始位置 = evt.pos()
+        self.rb.setGeometry(QRect(self.起始位置, QSize()))  # 起始位置 + 空尺寸
+        # 展示橡皮筋控件
+        self.rb.show()
+
+    def mouseMoveEvent(self, evt):
+        '''鼠标移动时执行'''
+        # 调整橡皮筋控件的位置和尺寸
+        self.rb.setGeometry(QRect(self.起始位置, evt.pos()).normalized())  # 起始位置 + 鼠标当前位置  normalized() 自动识别尺寸
+
+    def mouseReleaseEvent(self, evt):
+        '''鼠标松开时执行'''
+        # 获取橡皮筋控件的位置和尺寸
+        rect = self.rb.geometry()
+        # 遍历所有控件, 康康哪个控件在橡皮筋控件内
+        for i in self.children():
+            if rect.contains(i.geometry()) and i.inherits('QCheckBox'):  # 包含在橡皮筋控件内 并且是 QCheckBox 控件
+                i.toggle()
+        # 隐藏橡皮筋控件
+        self.rb.hide()
 
 
 if __name__ == "__main__":
