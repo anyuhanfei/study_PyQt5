5ed4f5441e0b6c3e4821c8c16fa7b17f032b6b78
anyuhanfei <1223050251@qq.com>
2020-05-09 17:17:17 +0800

    297~298 QErrorMessage

--- 297~298-QErrorMessage/297-QErrorMessage-简介.py
+++ 297~298-QErrorMessage/297-QErrorMessage-简介.py
@@ -1,10 +1,19 @@
 '''
 297-QErrorMessage-简介
 
+错误消息小部件由文本标签和复选框组成
+
+该复选框允许用户控制将来是否再次显示相同的错误消息
+
+继承自 QDialog
+
+构造函数:
+    QErrorMessage(parent: QWidget = None)
+
 '''
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QErrorMessage
 
 
 class Window(QWidget):
@@ -13,6 +22,9 @@ class Window(QWidget):
 
         self.setWindowTitle('297-QErrorMessage-简介')
         self.resize(1000, 500)
+
+        em = QErrorMessage(self)
+        em.show()
 
 
 if __name__ == "__main__":
--- 297~298-QErrorMessage/298-QErrorMessage-功能作用.py
+++ 297~298-QErrorMessage/298-QErrorMessage-功能作用.py
@@ -1,10 +1,14 @@
 '''
 298-QErrorMessage-功能作用
 
+API:
+    showMessage(str) -- 添加错误信息并展示, 展示的窗口是非模态窗口
+    qtHandler() -- 调用此方法后, 可以使用一些封装好的弹出框, 如 Pyqt5.QtCore.qDebug, Pyqt5.QtCore.qWarning
 '''
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QErrorMessage
+from PyQt5.QtCore import qDebug, qWarning
 
 
 class Window(QWidget):
@@ -14,6 +18,22 @@ class Window(QWidget):
         self.setWindowTitle('298-QErrorMessage-功能作用')
         self.resize(1000, 500)
 
+        em = QErrorMessage(self)
+        em.setWindowTitle('错误提示')
+
+        em.showMessage('这是错误信息')
+        em.showMessage('这是错误信息')
+        em.showMessage('这是错误信息')
+        em.showMessage('这是错误信息!')
+
+        '''模态窗口式打开'''
+        em.open()
+
+        '''封装好的弹框'''
+        QErrorMessage.qtHandler()
+        qDebug('xxxxxx')
+        qWarning('oooooo')
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -22,4 +42,3 @@ if __name__ == "__main__":
     window.show()
 
     sys.exit(app.exec_())
-
